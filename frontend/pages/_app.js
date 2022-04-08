1a29f33f025673f1912dedf3f2e8148fe5bb8251
ximepaparella <ximenapaparella@gmail.com>
2022-04-08 09:21:09 -0300

    Setup Header y Pages principales

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,15 @@
+import Header from './../components/Header'
+
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+  return  (
+    <>
+    <Header/>
+    <main>
+      <Component {...pageProps} />∫
+    </main>
+  </>
+  )}
 
 export default MyApp
