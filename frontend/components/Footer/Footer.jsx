055e7279537964b1c083d27b41ab26fdcc97fe8f
ximepaparella <ximenapaparella@gmail.com>
2022-04-26 09:22:53 -0300

    Cambio de import de tipografia, ajustes menu

--- frontend/components/Footer/Footer.jsx
+++ frontend/components/Footer/Footer.jsx
@@ -1,7 +1,29 @@
+import Image from "next/image";
 import styles from "./Footer.module.scss";
 
 const Footer = () => {
-  return <footer className={styles.footer}> Footer </footer>;
+  return (
+    <footer className={styles.footer}>
+      <div className="col-6">
+        <Image
+          width="62"
+          height="29"
+          src="/crg-chico.png"
+          alt="Universidad de Ciencias Exactas de la Ciudad de Buenos Aires"
+        />
+      </div>
+      <div className="col-6">
+        <a href="">
+          <Image
+            width="180"
+            height="28"
+            src="/exactas-chico.png"
+            alt="Centro de Recursos Genético"
+          />
+        </a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- frontend/components/Head/Head.jsx
+++ frontend/components/Head/Head.jsx
@@ -9,12 +9,6 @@ const HeadSettings = () => {
         content="Centro de Recursos Genéticos de la Universidad de Ciencias Exactas de la ciudad de Buenos Aires."
       />
       <link rel="icon" href="/favicon.ico" />
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Archivo+Narrow:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap"
-        rel="stylesheet"
-      />
     </Head>
   );
 };
