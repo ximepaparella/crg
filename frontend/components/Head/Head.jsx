1cc6c1e139f9d0cbd72d66ba9c0265330e67536a
ximepaparella <ximenapaparella@gmail.com>
2022-04-26 10:10:32 -0300

    mejoras footer

--- frontend/components/Head/Head.jsx
+++ frontend/components/Head/Head.jsx
@@ -9,6 +9,10 @@ const HeadSettings = () => {
         content="Centro de Recursos Genéticos de la Universidad de Ciencias Exactas de la ciudad de Buenos Aires."
       />
       <link rel="icon" href="/favicon.ico" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no"
+      />
     </Head>
   );
 };
